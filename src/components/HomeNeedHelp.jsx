a4cb9c31b19176ac08d8e6f4a3152a01dd69ccb7
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-02-04 12:30:36 +0100

    home need help ok

--- src/components/HomeNeedHelp.jsx
+++ src/components/HomeNeedHelp.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function HomeNeedHelp() {
   return (
     <div className="home-need-help">
-      <h1>Potrzebujesz pomocy w sprawie dostępności strony internetowej?</h1>
-      <h2>Eybierz usługi naszej firmy!</h2>
-      <h3>
-        Zadzwoń do nas na numer XXX-XXX-XXX lub napisz na
-        kontakt@bardziejdostepni.pl
-      </h3>
+      <div className="home-need-help__wrapper">
+        <h1>Potrzebujesz pomocy w sprawie dostępności strony internetowej?</h1>
+        <h2>Wybierz usługi naszej firmy!</h2>
+        <h3>
+          Zadzwoń do nas na numer <span>XXX-XXX-XXX</span> lub napisz na
+          <span> kontakt@bardziejdostepni.pl</span>
+        </h3>
+      </div>
     </div>
   );
 }
