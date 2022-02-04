d5eb93edc8d7f22e0550bbe74e59b2ff774f81bd
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-02-04 11:52:59 +0100

    home links ok

--- src/components/HomeLinks.jsx
+++ src/components/HomeLinks.jsx
@@ -5,25 +5,26 @@ import { RiFilePaperLine } from "react-icons/ri";
 function HomeLinks() {
   return (
     <div className="home-links">
-      <div className="home-links__law">
+      <div className="home-links__law home-links__container">
         <div className="img">
           <GoLaw />
         </div>
         <h1>Kto musi zadbać o dostępność cyfrową?</h1>
         <h2>
-          We wrześniu 2021 roku weszły zmian, które wiele zmieniają na tym polu.
+          We wrześniu 2021 roku weszły zmiany, które wiele zmieniają na tym
+          polu.
         </h2>
-        <button>Czytaj</button>
+        <button>Czytaj więcej</button>
       </div>
-      <div className="home-links__requirements">
+      <div className="home-links__requirements home-links__container">
         <div className="img">
           <RiFilePaperLine />
         </div>
-        <h1>Jakie wymogi należy spełnić?</h1>
+        <h1>Jakie standardy powinniśmy spełnić aby być dostępni cyfrowo?</h1>
         <h2>
           Wymogi zawarte są w standardzie WCAG 2.1, warto się z nim zapoznać.
         </h2>
-        <button>Czytaj</button>
+        <button>Czytaj więcej</button>
       </div>
     </div>
   );
